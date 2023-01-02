98fabef6edf2e96022d0649b5b6aec31f39193c3
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2023-01-02 21:59:36 +0200

    Changed source code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to the CI/CD App</p>
+        <p>Welcome to the Monday group :D</p>
       </header>
     </div>
   );
